bbf3eaa49df7473806206e07b9283ea4f6c86870
Satyendra kumar <Satyendrakumar@bhavnacorp.com>
2018-06-05 15:23:55 +0530

    JavaProgramForInterview

---
+++ Test/src/string/tutorial/StringTest.java
@@ -0,0 +1,11 @@
+package string.tutorial;
+
+public class StringTest {
+public static void main(String[] args) {
+    
+    // 
+    String s="ram"; // string leteral
+    String s1= new String("ram"); // string with new key words
+}
+    
+}
